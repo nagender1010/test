b4bc5311e54508e3bf3839f6d063c057d3bf93dd
Nagender <nagenderteegala@gmail.com>
2017-10-08 11:46:50 -0500

    Initial Checkin of GitTest

---
+++ GitTest/src/com/umkc/Main.java
@@ -0,0 +1,15 @@
+package com.umkc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		m1();
+		 
+	}
+
+	private static void m1() {
+		System.out.println("checking git");
+	}
+
+}
